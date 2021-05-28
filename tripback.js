3191cfb68f5c559597470fa80cee02d4616ea8a3
BA_CalderonMorales <tbcm640@gmail.com>
2021-05-28 12:26:51 -0500

    Added functions to hold previous while loops. Flow control improved.

--- tripback.js
+++ tripback.js
@@ -1,72 +1,118 @@
 "use-strict"
 
+/* #region Arrays to be Used */
 let destination = ['Nebraska', 'Wyoming', 'Hawaii', 'Alaska'];
 let restaurant = ['IHop', 'Taco Bell', 'McDonalds', 'Texas Roadhouse'];
 let transportation = ['Car', 'Van', 'SUV', 'Motorcycle'];
 let entertainment = ["Salsa lessons", "Scuba Diving", "Formula 1 Racing", "Para-gliding"];
+/* #endregion */
 
-let theFlag = true;
-
+/* #region User choices */
 let userDestination;
 let userRestaurant;
 let userTransportation;
 let userEntertainment;
+/* #endregion */
 
+/* #region Boolean flags to help with flow control */
+let theFlag = true;
+let theDestinationFlag = true;
+let theRestaurantFlag = true;
+let theTransportationFlag = true;
+let theEntertainmentFlag = true;
+/* #endregion */
 
-while(theFlag){
+while (theFlag) {
 
-    let theDestinationFlag = true;
+    let theChosenDestination = prompt("Choose a destination: Nebraska, Wyoming, Hawaii, Alaska");
+    chooseADestination(theChosenDestination);
+    console.log(userDestination);
 
-    while (theDestinationFlag){
-        let theChosenDestination = prompt("Choose a destination: Nebraska, Wyoming, Hawaii, Alaska");
-        for (let i = 0; i < destination.length; i++){
-            if (theChosenDestination === destination[i]){
-                userDestination = destination[i];
-                theDestinationFlag = false;
-                console.log(userDestination);
-            }
-        }
-    }
+    let theChosenRestaurant = prompt("Choose a restaurant: IHop, Taco Bell, McDonalds, Texas Roadhouse");
+    chooseARestaurant(theChosenRestaurant);
+    console.log(userRestaurant);
 
-    let theRestaurantFlag = true;
+    let theChosenTransportation = prompt("Choose a form of transportation: Car, Van, SUV, Motorcycle");
+    chooseATranspo(theChosenTransportation);
+    console.log(userTransportation);
 
-    while (theRestaurantFlag){
-        let theChosenRestaurant = prompt("Choose a restaurant: IHop, Taco Bell, McDonalds, Texas Roadhouse");
-        for (let i = 0; i < restaurant.length; i++){
-            if (theChosenRestaurant === restaurant[i])
-            {
-                userRestaurant = restaurant[i];
-                theRestaurantFlag = false;
-                console.log(userRestaurant);
-            }          
-        }
-    }
-
-    let theTransportationFlag = true;
-
-    while(theTransportationFlag){
-        let theChosenTransportation = prompt("Choose a form of transportation: Car, Van, SUV, Motorcycle");
-        for (let i = 0; i < transportation.length; i++) {
-            if (theChosenTransportation === transportation[i]){
-                userTransportation = transportation[i];
-                theTransportationFlag = false;
-                console.log(userTransportation);
-            }
-        }
-    }
-
-    let theEntertainmentFlag = true;
-
-    while (theEntertainmentFlag){
-        let theChosenEntertainment = prompt("Choose the preferred entertainment: Salsa lessons, Scuba Diving, Formula 1 Racing, Para-gliding");
-        for (let i = 0; i < entertainment.length; i++){
-            if (theChosenEntertainment === entertainment[i]){
-                userEntertainment = entertainment[i];
-                theEntertainmentFlag = false;
-                console.log(userEntertainment);
-            }
-        }
-    }
+    let theChosenEntertainment = prompt("Choose the preferred entertainment: Salsa lessons, Scuba Diving, Formula 1 Racing, Para-gliding");
+    chooseAEntertainment(theChosenEntertainment);
+    console.log(userEntertainment);
 
     theFlag = false;
+}
+
+let didUserLikeEverything = prompt("Did you like everything you chose? Please type enter 'yes' or 'no'.");
+let areYouSure = true;
+let theCurrentChange;
+
+while (areYouSure) {
+    switch (didUserLikeEverything) {
+        case "yes":
+            console.log("complete");
+            areYouSure = false;
+            break;
+        case "no":
+            console.log("Sorry!");
+            theCurrentChange = prompt("What did you want to change? Please type any of the following: Destination, Restaurant, Transportation, or Entertainment");
+            let areYouSure = someChange(theCurrentChange);
+            break;
+        default:
+            console.log("That is not a valid input");
+            break;
+    }
+}
+
+function chooseADestination(userDestinationInput) {
+    while (theDestinationFlag) {
+        for (let i = 0; i < destination.length; i++) {
+            if (userDestinationInput === destination[i]) {
+                userDestination = destination[i];
+                theDestinationFlag = false;
+            }
+        }
+    }
+    return userDestination;
+}
+
+function chooseARestaurant(userRestaurantInput) {
+    while (theRestaurantFlag) {
+        for (let i = 0; i < restaurant.length; i++) {
+            if (userRestaurantInput === restaurant[i]) {
+                userRestaurant = restaurant[i];
+                theRestaurantFlag = false;
+            }
+        }
+    }
+    return userRestaurant;
+}
+
+function chooseATranspo(userTransportationInput) {
+    while (theTransportationFlag) {
+        for (let i = 0; i < transportation.length; i++) {
+            if (userTransportationInput === transportation[i]) {
+                userTransportation = transportation[i];
+                theTransportationFlag = false;
+            }
+        }
+    }
+    return userTransportation;
+}
+
+function chooseAEntertainment(userEntertainmentInput) {
+    while (theEntertainmentFlag) {
+        for (let i = 0; i < entertainment.length; i++) {
+            if (userEntertainmentInput === entertainment[i]) {
+                userEntertainment = entertainment[i];
+                theEntertainmentFlag = false;
+            }
+        }
+    }
+    return userEntertainment;
+}
+
+function someChange(change) {
+    console.log(change);
+    areYouSure = false;
 }
